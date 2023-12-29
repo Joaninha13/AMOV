5a09f9698035597aaff432d7556accbd19b0a3cd
sudo-john-blossom <73914719+sudo-joao-carvalho@users.noreply.github.com>
2023-12-29 23:03:15 +0000

    PopUp Showing on longPress

--- app/src/main/java/pt/isec/ans/amov/ui/Components/MapScreen.kt
+++ app/src/main/java/pt/isec/ans/amov/ui/Components/MapScreen.kt
@@ -1,14 +1,32 @@
 package pt.isec.ans.amov.ui.Components
 
 
+import android.app.Activity
+import android.content.Intent
+import android.gesture.GestureOverlayView
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
+import android.os.Handler
+import android.provider.MediaStore
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,20 +39,28 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
+import org.osmdroid.events.MapEventsReceiver
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.MapEventsOverlay
 import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import pt.isec.ans.amov.R
+import pt.isec.ans.amov.ui.Components.PopUps.PopUpBase
+import pt.isec.ans.amov.ui.Screens.LocationFormState
+import pt.isec.ans.amov.ui.Screens.TextInputs
 import pt.isec.ans.amov.ui.ViewModels.FireBaseViewModel
 import pt.isec.ans.amov.ui.ViewModels.LocationViewModel
-
+import pt.isec.ans.amov.ui.theme.BlueLighter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,7 +70,7 @@ fun MapScreen(
     buttonToCenterClicked : Boolean,
     handleButtonToCenterClicked : (Boolean) -> Unit
 ){
-
+    var showPopUp by remember { mutableStateOf(false) }
     var autoEnabled by remember{ mutableStateOf(false) }
     val location = viewModelL.currentLocation.observeAsState()
 
@@ -95,9 +121,25 @@ fun MapScreen(
                 .clipToBounds()
                 .background(Color(255, 240, 128)),
         ) {
+            var mapView by remember { mutableStateOf<MapView?>(null) }
+
             AndroidView(
                 factory = { context ->
-                    MapView(context).apply {
+
+                    /*val mReceive = object : MapEventsReceiver {
+                        override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
+                            return false
+                        }
+
+                        override fun longPressHelper(p: GeoPoint?): Boolean {
+                            p?.let {
+                                Toast.makeText(context, "${p.latitude} - ${p.longitude}", Toast.LENGTH_LONG).show()
+                            }
+                            return true  // Retorna true para indicar que o evento foi tratado
+                        }
+                    }*/
+
+                    mapView = MapView(context).apply {
                         setTileSource(TileSourceFactory.MAPNIK);//==TileSourceFactory.DEFAULT_TILE_SOURCE
                         setMultiTouchControls(true)
                         controller.setCenter(geoPoint)
@@ -129,7 +171,59 @@ fun MapScreen(
                                 setPersonIcon(defaultDrawable)
                             }
                         )
+
+                        /*val gestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+
+                            override fun onDown(e: MotionEvent): Boolean {
+                                return e.pointerCount == 1
+                            }
+
+                            override fun onLongPress(e: MotionEvent){
+                                if (e.pointerCount == 1) {
+                                    val x = e.x
+                                    val y = e.y
+                                    val touchedGeoPoint = mapView?.projection?.fromPixels(x.toInt(), y.toInt())
+                                    // Lógica adicional, se necessário, ao detectar uma pressão longa
+                                    touchedGeoPoint?.let {
+                                        Toast.makeText(context, "${it.latitude} - ${it.longitude}", Toast.LENGTH_LONG).show()
+                                    }
+
+                                    // Chama performClick para lidar com acessibilidade
+                                    performClick()
+                                }
+                            }
+                        })*/
+
+                        // Adiciona o MapEventsReceiver diretamente ao MapView -> isto serve para quando a pessoa pressionar drante algum tempo seguido sejam obtidas as coordenadas do ponto onde carregou
+                        val mReceive = object : MapEventsReceiver {
+                            private val handler = Handler()
+                            private val longPressDuration = 2000L // Tempo de pressão longa em milissegundos (2 segundos)
+                            private var lastPressTime = 0L
+
+                            override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
+                                return false
+                            }
+
+                            override fun longPressHelper(p: GeoPoint?): Boolean {
+                                val currentTime = System.currentTimeMillis()
+                                if (currentTime - lastPressTime > longPressDuration) {
+                                    handler.postDelayed({
+                                        p?.let {
+                                            showPopUp = true
+                                            //Toast.makeText(context, "${it.latitude} - ${it.longitude}", Toast.LENGTH_LONG).show()
+                                        }
+                                    }, longPressDuration)
+                                    lastPressTime = currentTime
+                                }
+
+                                return true
+                            }
+
+                        }
+
+                        overlays.add(MapEventsOverlay(mReceive))
                     }
+                    mapView!!
                 },
                 update = { view ->
                     view.controller.setCenter(geoPoint)
@@ -141,35 +235,155 @@ fun MapScreen(
                 }
             )
         }
-        /*Spacer(Modifier.height(16.dp))
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            items(viewModel.POIs) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp),
-                    elevation = CardDefaults.cardElevation(4.dp),
-                    colors = CardDefaults.cardColors(
-                        containerColor = Color(128,192,255)
-                    ),
-                    onClick = {
-                        TODO("...")
-                    }
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(text = it.team, fontSize = 20.sp)
-                        Text(text = "${it.latitude} ${it.longitude}", fontSize = 14.sp)
-                    }
+
+        ShowPopUpBase(showPopUp){
+            showPopUp = false
+        }
+    }
+}
+
+@Composable
+fun ShowPopUpBase(showPopUp: Boolean, onDismiss: () -> Unit) {
+    var locationFormState by remember { mutableStateOf(LocationFormState()) }
+
+    val pickImageLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                //Log.d("VERRR------>", "result: $result") // passa aqui
+                result.data?.data?.let { uri ->
+                    // Aqui você tem a URI da imagem selecionada
+                    // Agora você pode fazer o upload para o Firestore ou atualizar o estado conforme necessário
+                    locationFormState.imageUri = uri
                 }
             }
-        }*/
+        }
+
+    if (showPopUp) {
+        PopUpBase(
+            showDialog = true,
+            title = "Add Location",
+            content = {
+                //First inputs
+                Column(
+                    modifier = Modifier
+                        .width(300.dp),
+                    verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.Top),
+                    horizontalAlignment = Alignment.Start,
+                ) {
+
+                    //Country
+                    Row(
+                        modifier = Modifier
+                            .width(300.dp)
+                            .height(60.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+
+                        OutlinedInput(
+                            _value = locationFormState.country,
+                            _label = "Country",
+                            _iconName = R.drawable.nameicon,
+                            onValueChange = { newValue ->
+                                locationFormState.country = newValue
+                            }
+                        )
+                    }
+
+                    //Region
+                    Row(
+                        modifier = Modifier
+                            .width(300.dp)
+                            .height(60.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+
+                        OutlinedInput(
+                            _value = locationFormState.region,
+                            _label = "Region",
+                            _iconName = R.drawable.nameicon,
+                            onValueChange = { newValue ->
+                                locationFormState.region = newValue
+                            }
+                        )
+                    }
+
+                    //Description
+                    Row(
+                        modifier = Modifier
+                            .width(300.dp)
+                            .height(60.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+
+                        OutlinedInput(
+                            _value = locationFormState.description,
+                            _label = "Description",
+                            _iconName = R.drawable.descicon,
+                            onValueChange = { newValue ->
+                                locationFormState.description = newValue
+                            }
+                        )
+
+                    }
+                }
+
+                //Upload Images
+                Row(
+                    modifier = Modifier
+                        .border(
+                            width = 1.dp,
+                            color = BlueLighter,
+                            shape = RoundedCornerShape(size = 5.dp)
+                        )
+                        .width(300.dp)
+                        .height(30.dp)
+                        .background(
+                            color = Color(0xCCFFFFFF),
+                            shape = RoundedCornerShape(size = 5.dp)
+                        )
+                        .padding(start = 10.dp, end = 10.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    ClickableText(
+                        text = buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.Blue)) {
+                                append("Upload Image")
+                            }
+                        },
+                        onClick = { offset ->
+                            // Iniciar a atividade de escolha de imagem da galeria
+                            pickImageLauncher.launch(
+                                Intent(
+                                    Intent.ACTION_PICK,
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                                )
+                            )
+                        },
+                        modifier = Modifier.clickable {
+                            // por aqui a foto que deu upload
+                        }
+                    )
+
+                    /*Text(
+                        text = "Upload Images",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            //fontFamily = FontFamily(Font(R.font.inter)),
+                            fontWeight = FontWeight(500),
+                            color = BlueSoft,
+                        )
+                    )*/
+                }
+                      },
+            buttonText = "Add",
+            onConfirm = {
+                // Implemente a lógica de confirmação
+            },
+            onDismiss = onDismiss
+        )
     }
 }
